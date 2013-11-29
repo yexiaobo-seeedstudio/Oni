a8509b7b72123156863b417737903777f11cc9ae
xiaobo <kfihihc@gmail.com>
2013-11-29 13:58:54 +0800

    update the usage tips

--- Library/oni.py
+++ Library/oni.py
@@ -1,5 +1,52 @@
 #!/usr/bin/env python
-# -*- coding:utf8 -*- #
+# -*- coding: utf8 -*-
+
+import os
+import sys
+
+def executeCommand( args ):
+    pass
+
+def usage():
+    u = """\
+Intro:
+    Oni is an simple Arduino library manager.
+
+Usage:
+    oni command [options] library ...
+
+command:
+    info    : information of the library
+    install : install library
+    remove  : remove library
+    list    : list all library which have been installed
+    search  : search what you want
+    home    : open the library homepage
+    config  : listing all Oni setup information
+
+example:
+    oni install Grove_Relay
+    """
+    return u
+
+def parseArgs():
+    pass
+
+def main():
+    '''
+    The entry for the program
+    '''
+    #TODO
+    # 1. parse args
+    # 2. implementing some commands: install remove info list search home
+    # config
+    # 2. get library's name
+
+    if len(sys.argv) < 2:
+        print usage()
+
+    args = parseArgs()
+    executeCommand(args)
 
 if __name__ == '__main__':
     main()
